0a777c965c12b75eb2a21dc47c8dc6635176a53f
bigC <814151789@qq.com>
2021-10-25 18:52:08 +0800

    update code

--- src/articleDetail/articleDetail.js
+++ src/articleDetail/articleDetail.js
@@ -1,52 +1,55 @@
-class Markdown{
+import article from '../article_route';
+import { parseHTML } from '../../assets/lib/utils/utils'
+
+
+
+class Markdown {
     static TITLE_REG = /(#+)\s+([^\n]+)/;
-    constructor(text){
+    constructor(text) {
         this.text = text;
     }
-    parse(){
-        return this.text.replace(/.+/g,(item)=>{
+    parse() {
+        return this.text.replace(/.+/g, (item) => {
             console.log(item);
-            if(Markdown.isTitle(item)){
+            if (Markdown.isTitle(item)) {
                 return this.parseTitle(item);
-            }else{
+            } else {
                 return this.parseParagraph(item);
             }
         })
     }
-    parseParagraph(text){
+    parseParagraph(text) {
         return text.trim().length ? `<p>${text.trim()}</p>` : '';
     }
-    parseTitle(text){
-        return text.replace(Markdown.TITLE_REG,function(item){
+    parseTitle(text) {
+        return text.replace(Markdown.TITLE_REG, function (item) {
             let n = RegExp.$1.length > 6 ? 6 : RegExp.$1.length;  // 1-6个#号；
             let textConent = RegExp.$2;
             return `<h${n}>${textConent}</h${n}>`;
         })
     }
-    static isTitle(val){
+    static isTitle(val) {
         return Markdown.TITLE_REG.test(val);
     }
 }
 
-//必须要拿到每一行； # helloworld
-// ##  我是h2
-// 我是
-// if(是#号开头){
 
-// }else {
-//     用P标签；
-// }
+(async function () {
+    let url = new URLSearchParams(window.location.search);
+    let id = url.get('id');
+    let data = article.filter(item => {
+        return item.id == id;
+    })[0];
+    let art = await data.article();
+    let html = parseHTML(art.default);
+    var el = document.querySelector('article');
+    el.appendChild(html);
 
-var el = document.querySelector('article');
-let textContent = el.textContent;
-// console.log(textContent)
-let md = new Markdown(el.textContent);
-let res = md.parse();
-el.innerHTML = res;
-
-// match方法是匹配所有符合正则表达式的语句，把匹配到的值放到数组中；
-// let res = s.match(/(#+\s*)|\S+|\n/g);
-// console.log(res);
+    //解析markdown语法
+    let md = new Markdown(el.textContent);
+    let res = md.parse();
+    el.innerHTML = res;
+})()
 
 
 
--- src/home/home.js
+++ src/home/home.js
@@ -1,35 +1,17 @@
 import articles from '../article_route'
+import { parseHTML } from '../../assets/lib/utils/utils';
+
+
 var tabItem = document.querySelector('.tab-item');
 var main = tabItem.querySelector('.main');
 var temp = tabItem.querySelector('.item');
 temp.remove();
+
+//渲染文章列表
 articles.forEach(async article => {
     var cloneNode = temp.cloneNode(true);
-    let articleContent = await article.article();
-    cloneNode.insertBefore(articleContent, cloneNode.querySelector('.nav'));
+    let art = await article.article();
+    let html = parseHTML(art.default);
+    cloneNode.insertBefore(html, cloneNode.querySelector('.nav'));
     main.appendChild(cloneNode);
 })
-
-    // var str = 'hello world'; // new String()
-    // var reg = /\n/  //正则：是用来匹配字符串的；
-    //但是，有一些特殊字符，是无法匹配的；
-    // 特殊字符： / \ () [] {} \n
-    //  \n 匹配回车；
-    // \s 匹配空格；
-    // g  global;   
-    //  +  一个或者多个
-
-    // var s = '#############'
-    //  let res = s.replace(/#+(\S)*\s/g, function(item){
-    //      console.log(item) //#  ## ### ####  ##### ######
-    //      if(item.length)
-    //      var el = document.createElement('h' + item.length);
-    //      return item;
-    //  })
-
-    // 作业： 
-    // 1.匹配换行，把换行之前的文本改成p标签
-    // 2. ###  h3  
-    // #### h4  
-    // # h1
-    // ###### h6
